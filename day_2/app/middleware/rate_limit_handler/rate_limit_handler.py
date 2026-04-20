c3f6f7a653d37e556aca5a313868cf608cfbcb7d
SagarBhausahebSahane <sbsahane57@gmail.com>
2026-04-20 19:11:02 +0530

    Remove day_2 folder from remote

--- day_2/app/middleware/rate_limit_handler/rate_limit_handler.py
+++
@@ -1,4 +0,0 @@
-from slowapi import Limiter
-from app.utils.rate_limit_handler.rate_limit_handler import get_rate_limit_key
-from app.core.config.config import settings
-limiter = Limiter(key_func=get_rate_limit_key,storage_uri=settings.REDIS_URL,default_limits=["2/hour"],strategy="fixed-window")
